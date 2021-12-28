f46c7691313078dc79432739142822de20ec8eed
David <David>
2021-12-28 16:51:12 +0100

    k

---
+++ nn.py
@@ -0,0 +1,7 @@
+#In[]
+import pandas as pd
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+#In[]
+df = pd.read_excel(r"C:\Users\dadbc\Desktop\Phy\Repositorios\Test\regression_data.xls")
